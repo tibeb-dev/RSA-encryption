5e09988e84a08dba0076dbd9c5d4e02fbfe959b0
kidus sintayehu <88981542+kidussintayehu@users.noreply.github.com>
2021-09-28 19:50:19 +0300

    Add files via upload

---
+++ RSAalgorithm.py
@@ -0,0 +1,14 @@
+import random
+
+def convert_to_ascii(ciphertext):
+    cipherArray = []
+    i = 0
+    while i < len(str(ciphertext)):
+        if i == len(str(ciphertext)) - 1:
+            cipherArray.append(int(str(ciphertext)[i]))
+            return cipherArray
+        cipherArray.append(int(str(ciphertext)[i]) * 10 + int(str(ciphertext)[i+1]))
+        i += 2
+    return cipherArray
+
+
